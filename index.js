a31e28176e7ec0ebaf92aede8badc8590cc6e45c
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2022-09-05 18:22:12 +0000

    structure
    Creating the skeleton of the app

---
+++ index.js
@@ -0,0 +1,8 @@
+/when click is mage to show content in droplist/ 
+function myFunction(){
+    document.getElementsById("mydropdown").classList.toggle("show")
+}
+/to close droplist when user is outside it/ 
+window.onclick = function(e){
+    
+}
